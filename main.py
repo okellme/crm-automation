a9f931816580a22b2b83eb5d43c5821ce4eaf395
John Paul Salcedo <tiktokv806@gmail.com>
2026-08-07 10:11:53 +0800

    Add function to create new leads

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-leads = [
-    {"name": "Paul", "email": "Paul@email.com", "status": "active"},
-    {"name": "Maria", "email": "Maria@email.com", "status": "Contracted"},
-]
+def add_lead(leads, name, email, status):
+    new_lead = {"name": name, "email": email, "status": status}
+    leads.append(new_lead)
+
+leads = []
+
+add_lead(leads, "Paul", "paul@email.com", "New")
+add_lead(leads, "Maria", "maria@email.com", "Contacted")
 
 for lead in leads:
     print(f"{lead['name']} - {lead['status']}")
-
