39d0ac9a8e4b382e93828c4b707636f07d1d7e2b
John Paul Salcedo <tiktokv806@gmail.com>
2026-08-08 19:53:10 +0800

    Clean up test files and add requirements.txt

--- error_test.py
+++
@@ -1,8 +0,0 @@
-try:
-    name = input("Enter lead name: ")
-    while name.strip() == "":
-        print("Name cannot be empty.")
-        name = input("Enter lead name: ")
-    print(f"You entered {number}")
-except ValueError:
-    print("That's not a valid number. Please try again.")
